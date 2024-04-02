404c004ca2f928cdc436854b22f9521baee65bb6
unknown <xaliloff209@gmail.com>
2024-04-02 07:59:06 +0400

    js task

---
+++ app.js
@@ -0,0 +1,83 @@
+//First Question
+// let number = 18;
+// let firstNumber = 1;
+//let lastNumber = 8;
+//console.log(firstNumber, " ", +lastNumber); //
+
+//Second Question
+//let number = 198;
+//let lastNumber = number % 10;
+//let firstNumber = parseInt(number / 100);
+//console.log(firstNumber * 10 + lastNumber); //
+
+//3 Question
+//let x = 4;
+//let y = 15;
+//console.log(x % y);
+
+//6thquestion
+//let length = 6; //
+//let width = 5; //
+//let perimeter; //
+//constant findPerimeter(l, w) {
+// return 2 * (l + w);}
+//perimeter = findPerimeter(length, width);
+//console.log(
+//"The perimeter of a rectangle with length " + length +" units and width " +    width +    " units is: " +    perimeter +   " units.");
+
+//let side = 4;
+//function findPerimeter(side) {
+// return 4 * side;}
+//perimeter = findPerimeter(4 * side);
+//console.log(4 * 4);
+
+//7th question
+//let firstNumber = 25;
+//let lastNumber = 85;
+//console.log(lastNumber * firstNumber);
+
+//8th question
+//let x = 11;
+//let y = 5;
+//console.log(x ** y);
+
+//9th question
+//let number = 12345;
+//console.log(
+// Math.floor(12345 / 10) );
+
+//10th question
+//let number = 20;
+//console.log(number % 10);
+
+//let number = 6789;
+
+// Initializing the result variable
+//let result = 0;
+
+//while(number>0){
+// Getting the rightmost digit
+//rightmost = number%10;
+
+//result = result*10 + rightmost;
+
+// Removing the rightmost digit from the number
+//number = Math.floor(number/10);}
+//console.log("Reversed number is : " + result)
+
+//12 Question
+//let number = 50;
+//let price = 10;
+//console.log(number / price);
+
+//13 question
+//let x = 2;
+//let s = 6 * x * x;
+//let v = x * x * x;
+//console.log(v);
+
+//14 question
+let number = 150;
+console.log(number / 10);
+number = parseInt(4 + number / 10);
+//???
